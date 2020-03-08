309b21b7ca442b47f168dda0d7722768e9171635
Moosees <linus.almgren@gmail.com>
2020-03-08 21:22:25 +0100

    Add helper objects for notes and intervals

---
+++ src/helpers/intervals.js
@@ -0,0 +1,137 @@
+export default {
+  0: {
+    name: 'Perfect Unison',
+    shortName: 'P1',
+    color: '#000'
+  },
+  1: {
+    name: 'Minor Second',
+    shortName: 'm2',
+    color: '#5D00FC'
+  },
+  2: {
+    name: 'Major Second',
+    shortName: 'M2',
+    color: '#006CFC'
+  },
+  3: {
+    name: 'Minor Third',
+    shortName: 'm3',
+    color: '#FECB00'
+  },
+  4: {
+    name: 'Major Third',
+    shortName: 'M3',
+    color: '#FE4C02'
+  },
+  5: {
+    name: 'Perfect Fourth',
+    shortName: 'P4',
+    color: '#02FFFF'
+  },
+  6: {
+    name: 'Tritone',
+    shortName: 'TT',
+    color: '#CAFE00'
+  },
+  7: {
+    name: 'Perfect Fifth',
+    shortName: 'P5',
+    color: '#FD1601'
+  },
+  8: {
+    name: 'Minor Sixth',
+    shortName: 'm6',
+    color: '#FEFF02'
+  },
+  9: {
+    name: 'Major Sixth',
+    shortName: 'M6',
+    color: '#FEA300'
+  },
+  10: {
+    name: 'Minor Seventh',
+    shortName: 'm7',
+    color: '#010FFF'
+  },
+  11: {
+    name: 'Major Seventh',
+    shortName: 'M7',
+    color: '#BB00FF'
+  },
+  12: {
+    name: 'Perfect Octave',
+    shortName: 'P8',
+    color: '#FF0000'
+  },
+  13: {
+    name: 'Minor Ninth',
+    shortName: 'm9',
+    color: '#E100EE'
+  },
+  14: {
+    name: 'Major Ninth',
+    shortName: 'M9',
+    color: '#0000FE'
+  },
+  15: {
+    name: 'Minor Tenth',
+    shortName: 'm10',
+    color: '#7EFD03'
+  },
+  16: {
+    name: 'Major Tenth',
+    shortName: 'M10',
+    color: '#FEFF02'
+  },
+  17: {
+    name: 'Perfect Eleventh',
+    shortName: 'P11',
+    color: '#1DFF00'
+  },
+  18: {
+    name: 'Augmented Eleventh',
+    shortName: 'A11',
+    color: '#00FD8A'
+  },
+  19: {
+    name: 'Perfect Twelfth',
+    shortName: 'P12',
+    color: '#00FE00'
+  },
+  20: {
+    name: 'Minor Thirteenth',
+    shortName: 'm13',
+    color: '#01FEE9'
+  },
+  21: {
+    name: 'Major Thirteenth',
+    shortName: 'M13',
+    color: '#01A2FD'
+  },
+  22: {
+    name: 'Minor Fourteenth',
+    shortName: 'm14',
+    color: '#9C00FD'
+  },
+  23: {
+    name: 'Major Fourteenth',
+    shortName: 'M14',
+    color: '#4000FD'
+  },
+  24: {
+    name: 'Second Octave',
+    shortname: 'P15',
+    color: '#FF0028'
+  },
+  other: {
+    name: 'Other',
+    shortName: 'X',
+    color: '#FF00FF'
+  },
+  bass: {
+    name: 'Lower',
+    shortName: 'Sub',
+    color: '#CC004A'
+  }
+};
