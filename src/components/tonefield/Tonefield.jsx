fa046c6d67c14ea5df6eb8a5a4f8d9cc7e02322d
Moosees <linus.almgren@gmail.com>
2020-03-21 21:47:54 +0100

    add audio test to tonefield

--- src/components/tonefield/Tonefield.jsx
+++ src/components/tonefield/Tonefield.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 
-const Tonefield = ({ color = '#000', text = '', note, isDing, position }) => (
-  <g
-    onClick={() => console.log({ note })}
-    cx="0"
-    cy="0"
-    transform={isDing ? '' : `rotate(${position + 90}) translate(7)`}
-    style={{ fontSize: isDing ? '0.35rem' : '0.25rem', cursor: 'pointer' }}
-  >
-    <circle r={isDing ? '2.5' : '2'} fill={color} />
-    <text
-      textAnchor="middle"
-      dy="0.3em"
-      fill="#ccc"
-      transform={isDing ? '' : `rotate(-${position + 90})`}
+const Tonefield = ({ color = '#000', text = '', note, isDing, position }) => {
+  return (
+    <g
+      onClick={() => new Audio('audio/rav/test.wav').play()}
+      cx="0"
+      cy="0"
+      transform={isDing ? '' : `rotate(${position + 90}) translate(7)`}
+      style={{ fontSize: isDing ? '0.35rem' : '0.25rem', cursor: 'pointer' }}
     >
-      {text}
-    </text>
-  </g>
-);
+      <circle r={isDing ? '2.5' : '2'} fill={color} />
+      <text
+        textAnchor="middle"
+        dy="0.3em"
+        fill="#ccc"
+        transform={isDing ? '' : `rotate(-${position + 90})`}
+      >
+        {text}
+      </text>
+    </g>
+  );
+};
 
 export default Tonefield;
