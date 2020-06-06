34395ec8909c79f6b087f202f43a51523e852c09
Moosees <linus.almgren@gmail.com>
2020-06-06 22:02:11 +0200

    Remove TextInput, update TODO

--- src/components/scaleInfo/ScaleInfo.jsx
+++ src/components/scaleInfo/ScaleInfo.jsx
@@ -1,34 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { setScaleName } from '../../redux/scale/scale.actions';
 import ButtonMain from '../button/ButtonMain';
 import InfoField from '../infoField/InfoField';
-import TextInput from '../textInput/TextInput';
 import { Buttons, InfoContainer } from './scaleInfo.styles';
 
 const ScaleInfo = ({ name, layout, scale }) => {
-  const [isEditing, setIsEditing] = useState(false);
-
   return (
-    <>
-      <InfoContainer>
-        <InfoField label={'Name: ' + name} />
-        <InfoField label={scale.join(', ')} />
-        <InfoField label={'Layout: ' + layout} />
-        <Buttons>
-          <ButtonMain label="Edit Info" onClick={() => setIsEditing(true)} />
-          <ButtonMain label="Save Scale" />
-        </Buttons>
-      </InfoContainer>
-      {isEditing && (
-        <div>
-          <TextInput value={name} setValue={setScaleName} />
-          <TextInput value={scale} disabled />
-          <TextInput value={layout} disabled />
-          <ButtonMain label="Confirm" onClick={() => setIsEditing(false)} />
-        </div>
-      )}
-    </>
+    <InfoContainer>
+      <InfoField label={'Name: ' + name} onEdit={true} />
+      <InfoField label={scale.join(', ')} onEdit={true} />
+      <InfoField label={'Layout: ' + layout} onEdit={true} />
+      <Buttons>
+        <ButtonMain label="Save Scale" />
+      </Buttons>
+    </InfoContainer>
   );
 };
 
