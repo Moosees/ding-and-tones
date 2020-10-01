b1606410d68e68a090265c8ecd91866f543d7787
Moosees <linus.almgren@gmail.com>
2020-10-01 22:31:14 +0200

    fix conditional check for song save limit middleware

--- middleware/user.js
+++ middleware/user.js
@@ -1,24 +1,26 @@
 const User = require('../models/user');
 const ObjectId = require('mongoose').Types.ObjectId;
 
-exports.checkSongsLimit = async (req, res, next) => {
-  if (!req.body.songId)
-    await User.aggregate()
-      .match({ _id: ObjectId(req.userId) })
-      .project({
-        numSavedSongs: { $size: '$songs' },
-        // maxSavedSongs: '$maxSavedSongs',
-      })
-      .exec((error, user) => {
-        console.log(user);
-        if (error) return res.status(400).json();
-        // if (user.numSavedSongs >= user.maxSavedSongs)
-        if (user.numSavedSongs >= 20)
-          return res.status(403).json({
-            message:
-              'Max number of saved songs reached, please delete a song before trying to save again',
-          });
-      });
+exports.checkSongsLimit = (req, res, next) => {
+  if (req.body.songId) return next();
 
-  next();
+  User.aggregate()
+    .match({ _id: ObjectId(req.userId) })
+    .project({
+      numSavedSongs: { $size: '$songs' },
+      maxSavedSongs: '$maxSavedSongs',
+    })
+    .exec((error, user) => {
+      const { numSavedSongs, maxSavedSongs } = user[0];
+
+      if (error) return res.status(400).json();
+
+      if (numSavedSongs >= maxSavedSongs)
+        return res.status(403).json({
+          message:
+            'Max number of saved songs reached, please delete a song before trying to save again',
+        });
+
+      next();
+    });
 };
