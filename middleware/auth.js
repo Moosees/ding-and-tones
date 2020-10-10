30c6de36a81632523cc1063f5ac4025a9bc4dad4
Moosees <linus.almgren@gmail.com>
2020-10-10 08:07:19 +0200

    fix bug that would skip checking userId when searching sometimes, refactor auth middleware code

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,47 +3,52 @@ const { OAuth2Client } = require('google-auth-library');
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
-exports.checkAuth = async (req, res, next) => {
+exports.checkAuth = (req, res, next) => {
   const authHeader = req.get('authorization');
-  if (!authHeader) return res.status(403).json({ msg: 'No authorization' });
+  const idToken = authHeader ? authHeader.split(' ')[1] : 'undefined';
 
-  const idToken = authHeader.split(' ')[1];
   if (idToken === 'undefined')
     return res.status(403).json({ msg: 'No authorization' });
 
-  const ticket = await client.verifyIdToken({
-    idToken,
-    audience: process.env.GOOGLE_CLIENT_ID,
-  });
-
-  const { sub } = ticket.getPayload();
-
-  User.findOne({ sub }, '_id', (error, user) => {
-    if (error || !user) return res.status(403).json({ msg: 'User not found' });
-
-    req.userId = user._id;
-    next();
-  });
-};
-
-exports.getUserId = async (req, res, next) => {
-  const authHeader = req.get('authorization');
-  if (!authHeader) return res.status(403).json({ msg: 'No authorization' });
-
-  const idToken = authHeader.split(' ')[1];
-  if (idToken !== 'undefined') {
-    const ticket = await client.verifyIdToken({
+  client
+    .verifyIdToken({
       idToken,
       audience: process.env.GOOGLE_CLIENT_ID,
-    });
+    })
+    .then((ticket) => {
+      const { sub } = ticket.getPayload();
 
-    const { sub } = ticket.getPayload();
+      User.findOne({ sub }, '_id', (error, user) => {
+        if (error || !user)
+          return res.status(403).json({ msg: 'User not found' });
 
-    await User.findOne({ sub }, '_id', (error, user) => {
-      if (error) return res.status(400).json();
-      if (user) req.userId = user._id;
-    });
-  }
+        req.userId = user._id;
+        next();
+      });
+    })
+    .catch((error) => res.status(403).json({ msg: 'No authorization' }));
+};
 
-  next();
+exports.getUserId = (req, res, next) => {
+  const authHeader = req.get('authorization');
+  const idToken = authHeader ? authHeader.split(' ')[1] : 'undefined';
+
+  if (idToken === 'undefined') return next();
+
+  client
+    .verifyIdToken({
+      idToken,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    })
+    .then((ticket) => {
+      const { sub } = ticket.getPayload();
+
+      User.findOne({ sub }, '_id', (error, user) => {
+        if (error || !user) return res.status(400).json();
+
+        req.userId = user._id;
+        next();
+      });
+    })
+    .catch((error) => res.status(403).json({ msg: 'No authorization' }));
 };
