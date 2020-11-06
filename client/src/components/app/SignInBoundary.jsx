9b992b58f52e3a36b4b4f1d1532586727eec5b57
Moosees <linus.almgren@gmail.com>
2020-11-06 09:56:05 +0100

    check if gapi script if loaded before trying to sign in user

--- client/src/components/app/SignInBoundary.jsx
+++ client/src/components/app/SignInBoundary.jsx
@@ -8,23 +8,38 @@ const SignInBoundary = ({ children, signIn, singOut }) => {
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    window.gapi.load('auth2', () => {
-      window.gapi.auth2
-        .init({
-          client_id: process.env.GOOGLE_CLIENT_ID || GOOGLE_CLIENT_ID,
-        })
-        .then(
-          (auth) => {
-            if (auth.isSignedIn.get()) signIn(auth.currentUser.get());
+    let tries = 0;
 
-            setLoaded(true);
-          },
-          (error) => {
-            signOut(error);
-            setLoaded(true);
-          }
-        );
-    });
+    const interval = setInterval(() => {
+      if (tries >= 100) {
+        clearInterval(interval);
+        setLoaded(true);
+      }
+
+      if (window.gapi) {
+        clearInterval(interval);
+
+        window.gapi.load('auth2', () => {
+          window.gapi.auth2
+            .init({
+              client_id: process.env.GOOGLE_CLIENT_ID || GOOGLE_CLIENT_ID,
+            })
+            .then(
+              (auth) => {
+                if (auth.isSignedIn.get()) signIn(auth.currentUser.get());
+
+                setLoaded(true);
+              },
+              (error) => {
+                signOut(error);
+                setLoaded(true);
+              }
+            );
+        });
+      } else {
+        ++tries;
+      }
+    }, 100);
   }, [signIn]);
 
   return <>{loaded ? children : <Loading />}</>;
