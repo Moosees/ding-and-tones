e5a4b0f13e52a82530d46ab0092e4139523294a3
Moosees <linus.almgren@gmail.com>
2020-12-11 23:08:37 +0100

    add functionality to bar info arrows to move bar in arrangement without dragging

--- client/src/components/arrangement/barInfo/BarInfo.jsx
+++ client/src/components/arrangement/barInfo/BarInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { metreList } from '../../../assets/metre';
+import { moveBarInArrangement } from '../../../redux/song/song.actions';
 import BtnIcon from '../../shared/button/Icon';
 import { BarInfoContainer, DragGradient } from './barInfo.styles';
 
@@ -10,6 +11,7 @@ const BarInfo = ({
   bars,
   beats,
   dragRef,
+  moveBarInArrangement,
   index,
   isDragging,
   isEditingSong,
@@ -19,7 +21,11 @@ const BarInfo = ({
 
   return (
     <BarInfoContainer>
-      <BtnIcon icon="navigate_before" />
+      <BtnIcon
+        icon="navigate_before"
+        disabled={index === 0}
+        onClick={() => moveBarInArrangement(index, index - 1)}
+      />
       <DragGradient
         disabled={isSongPlaying}
         ref={dragRef}
@@ -28,7 +34,11 @@ const BarInfo = ({
         Metre: {metreList[metre].nameShort} - Bar: {index + 1}/
         {arrangement.length}
       </DragGradient>
-      <BtnIcon icon="navigate_next" />
+      <BtnIcon
+        icon="navigate_next"
+        disabled={index === arrangement.length - 1}
+        onClick={() => moveBarInArrangement(index, index + 1)}
+      />
     </BarInfoContainer>
   );
 };
@@ -40,4 +50,4 @@ const mapStateToProps = ({ song, ui }) => ({
   isSongPlaying: ui.isSongPlaying,
 });
 
-export default connect(mapStateToProps)(BarInfo);
+export default connect(mapStateToProps, { moveBarInArrangement })(BarInfo);
