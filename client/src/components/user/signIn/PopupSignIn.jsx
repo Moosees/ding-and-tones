9a6ac9c5788cb8459b8f4d86a28cde0e80e1c7a6
Moosees <linus.almgren@gmail.com>
2020-12-19 21:56:48 +0100

    style sign in with Google button

--- client/src/components/user/signIn/PopupSignIn.jsx
+++ client/src/components/user/signIn/PopupSignIn.jsx
@@ -1,23 +1,10 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
 import { signIn, signOut } from '../../../redux/user/user.actions';
 import Popup from '../../shared/popup/Popup';
 import Privacy from '../../shared/privacy/Privacy';
-
-const SignInContainer = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  max-height: 80vh;
-  max-width: 60rem;
-  padding: 2rem;
-`;
-
-const TermsLink = styled.a`
-  text-decoration: underline;
-  padding-bottom: 1rem;
-`;
+import GoogleIcon from './GoogleIcon.jsx';
+import { GoogleBtn, SignInContainer, TermsLink } from './signIn.styles';
 
 const SignIn = ({ isSignedIn, onClose, signIn, signOut }) => {
   const [privacyOpen, setPrivacyOpen] = useState(false);
@@ -29,7 +16,10 @@ const SignIn = ({ isSignedIn, onClose, signIn, signOut }) => {
         <TermsLink onClick={() => setPrivacyOpen(true)}>
           terms and privacy policy.
         </TermsLink>
-        <button onClick={signIn}>Google Sign In</button>
+        <GoogleBtn onClick={signIn}>
+          <GoogleIcon />
+          <span>Sign in with Google</span>
+        </GoogleBtn>
         {privacyOpen && <Privacy />}
       </SignInContainer>
     </Popup>
