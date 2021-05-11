b5ffdede18b9e413f3dbbdeba4581e306ed9e98d
Moosees <linus.almgren@gmail.com>
2021-05-11 21:30:17 +0200

    create print chords Scale component

--- client/src/components/chordsPrint/scale/Scale.jsx
+++ client/src/components/chordsPrint/scale/Scale.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import MiniDrum from '../miniDrum/MiniDrum';
+import { connect } from 'react-redux';
+import { intervals } from '../../../assets/intervals';
+import MiniDrum from '../../drum/MiniDrum';
+import { InfoContainer, List, ListItem, ScaleContainer } from './scale.styles';
 
-const Scale = () => {
+const Scale = ({ info, scale }) => {
+  console.log({ info, scale });
+  const noteList = scale[0].intervalMap.map((note, i) => {
+    return <ListItem key={i}>{note.note}</ListItem>;
+  });
+
+  const intervalList = scale[0].intervalList.map((interval, i) => {
+    let intervalName = intervals[interval % 12].name;
+    if (interval % 12 === 0) intervalName = 'Octave';
+    if (interval === 0) intervalName = 'Root';
+
+    return <ListItem key={i}>{intervalName}</ListItem>;
+  });
+
   return (
-    <div>
-      <div>Scale name</div>
-      <MiniDrum />
-      <div>Intervals in scale</div>
-    </div>
+    <ScaleContainer>
+      <h1>{`${info.rootName} ${info.name}`}</h1>
+      <h3>{info.label}</h3>
+      <InfoContainer>
+        <MiniDrum />
+        <List>{noteList}</List>
+        <List>{intervalList}</List>
+      </InfoContainer>
+    </ScaleContainer>
   );
 };
 
-export default Scale;
+const mapStateToProps = ({ scale }) => ({
+  info: scale.info,
+  scale: scale.notes.scaleFull,
+});
+
+export default connect(mapStateToProps)(Scale);
---
+++ client/src/components/chordsPrint/scale/scale.styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const ScaleContainer = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const InfoContainer = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  margin: 10px 0 10px 25px;
+`;
+
+export const ListItem = styled.li`
+  list-style: none;
+`;
