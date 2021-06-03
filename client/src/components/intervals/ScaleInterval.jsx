5f86720854ef7cf02d61595354beec3d83e8c67b
Moosees <linus.almgren@gmail.com>
2021-06-03 22:16:32 +0200

    set interval range to one octave, fix intervals tooltip text, remove help text from interval list

--- client/src/components/intervals/ScaleInterval.jsx
+++ client/src/components/intervals/ScaleInterval.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { drumModes, intervals } from '../../assets/intervals';
+import { intervals } from '../../assets/intervals';
 import { setDisplayedNote } from '../../redux/drum/drum.actions';
 import {
   IntervalBreakBox,
@@ -10,26 +10,18 @@ import {
 } from './intervals.styles';
 
 const getIntervalProps = (interval) => {
-  const { semitones, compound, note } = interval;
+  const { compound, note, octaves, semitones } = interval;
 
-  const isInverted = semitones < 0;
-  const isCompound = semitones < 0 || semitones > 24;
-  const isBig = semitones < -12 || semitones > 24;
+  const isOctave = semitones % 12 === 0 && semitones !== 0;
 
-  const {
-    color,
-    colorInverted,
-    inverted,
-    invertedShort,
-    name,
-    nameShort,
-  } = intervals[isCompound ? compound : semitones];
+  const { color, name, nameShort } = intervals[isOctave ? 12 : compound];
+  const modifierBase = octaves === 0 ? '' : octaves > 0 ? '+' : '-';
 
   return {
-    color: isInverted ? colorInverted : color,
-    modifier: isBig ? (isInverted ? '-' : '+') : '',
-    name: isInverted ? inverted : name,
-    nameShort: isInverted ? invertedShort : nameShort,
+    color: color,
+    modifier: modifierBase.repeat(Math.abs(octaves)),
+    name: name,
+    nameShort: nameShort,
     note,
     semitones,
   };
@@ -37,50 +29,30 @@ const getIntervalProps = (interval) => {
 
 const ScaleInterval = ({
   displayedNote,
-  drumMode,
   interval,
   number,
   scaleIndex,
   setDisplayedNote,
 }) => {
-  const {
-    color,
-    modifier,
-    name,
-    nameShort,
-    note,
-    semitones,
-  } = getIntervalProps(interval);
+  const { color, modifier, name, nameShort, note, semitones } =
+    getIntervalProps(interval);
   const handleIntervalClick = () => {
     setDisplayedNote(displayedNote === scaleIndex ? 0 : scaleIndex);
   };
 
   return (
-    <IntervalContainer
-      intervals={drumMode === drumModes.INTERVALS}
-      onClick={handleIntervalClick}
-    >
+    <IntervalContainer onClick={handleIntervalClick}>
       <IntervalColor color={color}>
         <span>{number}</span>
       </IntervalColor>
       <IntervalBreakBox>
-        {drumMode === drumModes.INTERVALS && semitones === 0 ? (
-          <>
-            <IntervalText>{note} - Root</IntervalText>
-            <IntervalText>(click intervals</IntervalText>
-            <IntervalText>to change)</IntervalText>
-          </>
-        ) : (
-          <>
-            <IntervalText>
-              {note} - {name}
-              {modifier}
-            </IntervalText>
-            <IntervalText>
-              ({semitones} steps) - {nameShort}
-            </IntervalText>
-          </>
-        )}
+        <IntervalText>
+          {note} - {name}
+          {modifier}
+        </IntervalText>
+        <IntervalText>
+          ({semitones} steps) - {nameShort}
+        </IntervalText>
       </IntervalBreakBox>
     </IntervalContainer>
   );
@@ -88,7 +60,6 @@ const ScaleInterval = ({
 
 const mapStateToProps = ({ drum }) => ({
   displayedNote: drum.displayedNote,
-  drumMode: drum.drumMode,
 });
 
 export default connect(mapStateToProps, { setDisplayedNote })(ScaleInterval);
