3904bc80beec02348b4fd176ac815e30fac86170
Moosees <linus.almgren@gmail.com>
2021-08-09 22:31:14 +0200

    add debounced function to clear drum after playing drum with keyboard

--- client/src/hooks/useKeysPlayDrum.js
+++ client/src/hooks/useKeysPlayDrum.js
@@ -1,17 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { beatOptionToKeyCode } from '../assets/keyCodes';
 import { setCurrentBeat } from '../redux/ui/ui.actions';
 
 const useKeysPlayDrum = () => {
   const dispatch = useDispatch();
-  const sounds = useSelector(({ ui }) => ui.soundOptions.allSounds);
+  const { isSongPlaying, sounds } = useSelector(({ ui }) => ({
+    isSongPlaying: ui.isSongPlaying,
+    sounds: ui.soundOptions.allSounds,
+  }));
+  const [keypress, setKeypress] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (!isSongPlaying) dispatch(setCurrentBeat(null, []));
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [dispatch, keypress, isSongPlaying]);
 
   useEffect(() => {
     const keyboardCbs = Object.keys(sounds || {}).reduce((acc, soundKey) => {
       const cbKey = beatOptionToKeyCode[soundKey];
       const cbFunc = () => {
         dispatch(setCurrentBeat(null, soundKey));
+        setKeypress((keypress) => !keypress);
         new Audio(sounds[soundKey]).play();
       };
 
