af44886b45271c9cba668f88ac3185e8ee633f15
Moosees <linus.almgren@gmail.com>
2021-08-09 21:28:15 +0200

    add play button to drum route on smaller screens

--- client/src/components/intervals/Intervals.jsx
+++ client/src/components/intervals/Intervals.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { helpTopics } from '../../assets/help';
+import useDimensions from '../../hooks/useDimensions';
+import Buttons from '../shared/button/Buttons';
 import BtnHelp from '../shared/button/Help';
 import DividerLine from '../shared/dividerLine/DividerLine';
 import ScrollBox from '../shared/scrollBox/ScrollBox';
+import PlayButton from '../songControls/playButton/PlayButton';
 import ChordInterval from './ChordInterval';
 import DrumMode from './DrumMode';
 import { IntervalList } from './intervals.styles';
 import ScaleInterval from './ScaleInterval';
 
 const Intervals = ({ displayedChord, displayedNote, round, scale }) => {
+  const { isMobile } = useDimensions();
+
   const intervals =
     scale.length && displayedChord
       ? displayedChord.intervals.map((interval, i) => (
@@ -22,7 +27,7 @@ const Intervals = ({ displayedChord, displayedNote, round, scale }) => {
       : scale[displayedNote].intervalMap.map((interval, i) => {
           const { isExtra, localIndex } = scale[i];
           const number = isExtra ? `b${localIndex + 1}` : localIndex;
-          
+
           return (
             <ScaleInterval
               key={i}
@@ -35,7 +40,10 @@ const Intervals = ({ displayedChord, displayedNote, round, scale }) => {
 
   return (
     <IntervalList>
-      <BtnHelp topic={helpTopics.INTERVALS} />
+      <Buttons position="center">
+        {isMobile && <PlayButton light />}
+        <BtnHelp topic={helpTopics.INTERVALS} />
+      </Buttons>
       <DrumMode />
       <DividerLine small />
       <ScrollBox>{intervals}</ScrollBox>
