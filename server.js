2dc738021ee2bd67247abc28a581c493a14c6d41
Moosees <linus.almgren@gmail.com>
2021-09-07 21:42:39 +0200

    update server dependencies

--- server.js
+++ server.js
@@ -33,12 +33,7 @@ if (process.env.NODE_ENV === 'production') {
 
 // Database
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to database'))
   .catch((error) => console.error('Database connection failed: ', error));
 
