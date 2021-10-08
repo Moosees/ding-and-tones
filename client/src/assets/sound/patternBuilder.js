fe8389912207f4b5fdc4852c2086d85ea3f7a2ad
Moosees <linus.almgren@gmail.com>
2021-10-09 00:36:12 +0200

    clean up buildPatternFromSong function

--- client/src/assets/sound/patternBuilder.js
+++ client/src/assets/sound/patternBuilder.js
@@ -1,51 +1,58 @@
 import { metreList } from '../metre';
 import { store } from '../../redux/store';
 
-export const buildPatternFromSong = (howls) => {
+export const buildPatternFromBar = (barId, howls) => {
   const {
     song: {
-      arrangement,
       bars,
       beats,
       info: { bpm },
     },
+  } = store.getState();
+
+  const { measure, metre, subdivision } = bars[barId];
+  const { lengthInBeats } = metreList[metre];
+
+  const measureFiltered = measure.filter(
+    ({ beatId, value }) => beatId && value <= subdivision
+  );
+
+  const barDuration = (60000 / bpm) * lengthInBeats;
+  const beatDuration = barDuration / measureFiltered.length;
+
+  return measureFiltered.map(({ beatId, value }) => {
+    const { sound, mode, hand } = beats[beatId];
+    const play = () => {
+      sound.forEach((note) => {
+        if (note === '-' || !howls[note]) return;
+        howls[note].play();
+      });
+    };
+
+    return {
+      barId,
+      beatId,
+      sound,
+      mode,
+      hand,
+      play,
+      duration: beatDuration,
+    };
+  });
+};
+
+export const buildPatternFromSong = (howls) => {
+  const {
+    song: { arrangement },
     ui: { mutedBars },
   } = store.getState();
 
-  const pattern = [];
   const arrangementNonMuted = arrangement.filter((barId) => !mutedBars[barId]);
 
-  arrangementNonMuted.forEach((barId) => {
-    const { measure, metre, subdivision } = bars[barId];
-    const { lengthInBeats } = metreList[metre];
-
-    const measureFiltered = measure.filter(
-      ({ beatId, value }) => beatId && value <= subdivision
-    );
-
-    const barDuration = (60000 / bpm) * lengthInBeats;
-    const beatDuration = barDuration / measureFiltered.length;
-
-    measureFiltered.forEach(({ beatId, value }) => {
-      const { sound, mode, hand } = beats[beatId];
-      const play = () => {
-        sound.forEach((note) => {
-          if (note === '-' || !howls[note]) return;
-          howls[note].play();
-        });
-      };
-
-      pattern.push({
-        barId,
-        beatId,
-        sound,
-        mode,
-        hand,
-        play,
-        duration: beatDuration,
-      });
-    });
-  });
+  const pattern = arrangementNonMuted.reduce((acc, barId) => {
+    const bar = buildPatternFromBar(barId, howls);
+    return [...acc, ...bar];
+  }, []);
 
   console.log({ pattern });
   return pattern;
