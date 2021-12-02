855a35a7a098f7afb6721c44b93ceb1bee3c63dd
Moosees <linus.almgren@gmail.com>
2021-12-02 23:32:13 +0100

    add actions to add or remove single howls

--- client/src/redux/howls/howls.reducer.js
+++ client/src/redux/howls/howls.reducer.js
@@ -9,11 +9,19 @@ const INITIAL_STATE = {
 
 const howlsReducer = (state = INITIAL_STATE, { type, payload }) => {
   switch (type) {
+    case howlsTypes.ADD_HOWL:
+      console.log({ payload });
+      return state;
+
     case howlsTypes.CLEANUP_HOWLS:
       return { ...state, all: [], loadingStatus: {}, optionCbs: {} };
 
     case howlsTypes.CREATE_HOWLS:
       return { ...state, ...payload.howls };
+
+    case howlsTypes.REMOVE_HOWL:
+      console.log({ payload });
+      return state;
 
     case howlsTypes.SET_VOLUME:
       return { ...state, volume: payload.newVolume };
