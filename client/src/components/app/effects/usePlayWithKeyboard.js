f1fe67ff31784533dc12f775edb0427086bda235
Moosees <linus.almgren@gmail.com>
2022-07-01 22:23:16 +0200

    update playWithKeyboard to use new howls state structure

--- client/src/components/app/effects/usePlayWithKeyboard.js
+++ client/src/components/app/effects/usePlayWithKeyboard.js
@@ -1,28 +1,31 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { beatOptionToKeyCode } from '../../../assets/keyCodes';
 import { setCurrentlyPlaying } from '../../../redux/ui/ui.actions';
 
 const usePlayWithKeyboard = () => {
   const dispatch = useDispatch();
-  const howls = useSelector(({ howls }) => howls.all);
+  const { howls, scale } = useSelector(({ howls, scale }) => ({
+    howls: howls.data,
+    scale: scale.parsed.pitched,
+  }));
 
   useEffect(() => {
-    const keyboardCbs = howls?.length
-      ? howls.reduce((acc, howl) => {
-          const { key, option, play } = howl;
-          const cb = () => {
-            dispatch(
-              setCurrentlyPlaying({
-                currentHand: option === 't' ? 2 : 1,
-                currentSound: [option],
-              })
-            );
-            play();
-          };
+    const keyboardCbs = scale.reduce((acc, { note, option }) => {
+      const keyCode = beatOptionToKeyCode[option];
 
-          return { ...acc, [key]: cb };
-        }, {})
-      : {};
+      acc[keyCode] = () => {
+        dispatch(
+          setCurrentlyPlaying({
+            currentHand: option === 't' ? 2 : 1,
+            currentSound: [option],
+          })
+        );
+        howls[note].play();
+      };
+
+      return acc;
+    }, {});
 
     const keyboardListener = (e) => {
       if (!keyboardCbs[e.keyCode]) return;
@@ -34,7 +37,7 @@ const usePlayWithKeyboard = () => {
     return () => {
       document.removeEventListener('keydown', keyboardListener);
     };
-  }, [dispatch, howls]);
+  }, [dispatch, howls, scale]);
 };
 
 export default usePlayWithKeyboard;
