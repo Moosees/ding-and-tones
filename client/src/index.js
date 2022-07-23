21ab4ec5ec483524f19d9721b4a98aac6a85ba5f
Moosees <linus.almgren@gmail.com>
2022-07-24 01:49:11 +0200

    update client dependencies, update client index.js to account for react 18

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import React, { lazy, Suspense } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Loading from './components/shared/loading/Loading';
 import { API_ADDRESS } from './oauth';
 import * as serviceWorker from './serviceWorker';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
 
 const App = lazy(() => import('./components/app/App'));
 const GoogleResponse = lazy(() => import('./GoogleResponse'));
@@ -13,7 +16,7 @@ axios.defaults.baseURL = process.env.API_ADDRESS || API_ADDRESS;
 axios.defaults.headers['Accept'] = 'application/json';
 axios.defaults.withCredentials = true;
 
-ReactDOM.render(
+root.render(
   <Router>
     <Suspense fallback={<Loading />}>
       <Switch>
@@ -21,8 +24,7 @@ ReactDOM.render(
         <Route path="/" children={<App />} />
       </Switch>
     </Suspense>
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );
 
 // If you want your app to work offline and load faster, you can change
