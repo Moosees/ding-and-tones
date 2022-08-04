e8ad9c1c54ba56a3bb70ebc6217801b72daed4c5
Moosees <linus.almgren@gmail.com>
2022-08-04 23:26:31 +0200

    update MiniDrum MiniTonefield (when printing) to account for scale rotation

--- client/src/components/drum/tonefield/MiniTonefield.jsx
+++ client/src/components/drum/tonefield/MiniTonefield.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { getNoteLabelFromName } from '../../../assets/intervals';
 
 const MiniTonefield = ({ isDing, note, position, sharpNotes, showNote }) => {
+  const { scaleRotation } = useSelector(({ scale }) => ({
+    scaleRotation: scale.info.rotation,
+  }));
+
   const { rotate, translate } = position;
 
   return (
     <g
       cx="0"
       cy="0"
-      transform={`rotate(${rotate + 90}) translate(${translate})`}
+      transform={`rotate(${
+        rotate + scaleRotation + 270
+      }) translate(${translate})`}
       style={{
         fontSize: isDing ? '2.3' : '1.4',
         cursor: 'default',
@@ -24,7 +31,7 @@ const MiniTonefield = ({ isDing, note, position, sharpNotes, showNote }) => {
         textAnchor="middle"
         dy="0.3em"
         fill="#000"
-        transform={`rotate(-${rotate + 90})`}
+        transform={`rotate(-${rotate + scaleRotation + 270})`}
       >
         {showNote ? getNoteLabelFromName(note, sharpNotes) : ''}
       </text>
