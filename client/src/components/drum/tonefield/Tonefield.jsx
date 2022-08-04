6538c22541abbb63dfa714b68473935fa363a1a9
Moosees <linus.almgren@gmail.com>
2022-08-04 23:14:39 +0200

    update Drum Tonefields to account for scale rotation

--- client/src/components/drum/tonefield/Tonefield.jsx
+++ client/src/components/drum/tonefield/Tonefield.jsx
@@ -8,6 +8,7 @@ const Tonefield = ({
   isPlaying,
   localIndex,
   positionMap,
+  scaleRotation,
   showNote,
   text,
 }) => {
@@ -19,7 +20,9 @@ const Tonefield = ({
       onClick={showNote ? handlePlay : null}
       cx="0"
       cy="0"
-      transform={`rotate(${rotate + 90}) translate(${translate})`}
+      transform={`rotate(${
+        rotate + scaleRotation + 270
+      }) translate(${translate})`}
       style={{
         fontSize: isDing ? '2.3px' : '1.5px',
         cursor: showNote ? 'pointer' : 'default',
@@ -35,7 +38,7 @@ const Tonefield = ({
         textAnchor="middle"
         dy="0.3em"
         fill={isPlaying ? '#333' : '#ccc'}
-        transform={`rotate(-${rotate + 90})`}
+        transform={`rotate(-${rotate + scaleRotation + 270})`}
       >
         {text}
       </text>
@@ -45,6 +48,7 @@ const Tonefield = ({
 
 const mapStateToProps = ({ scale }) => ({
   positionMap: scale.parsed.positions,
+  scaleRotation: scale.info.rotation,
 });
 
 export default connect(mapStateToProps)(Tonefield);
