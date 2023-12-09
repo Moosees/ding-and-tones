9f72f8da959bfbdf62c2bdbdeeb7cd1f247afbbb
Moosees <linus.almgren@gmail.com>
2023-12-10 00:19:49 +0100

    update getUserId function to async await

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,7 +10,6 @@ exports.checkAuth = async (req, res, next) => {
   try {
     const user = await User.findById(req.session.user).select('_id').exec();
 
-    console.log('checkAuth', user);
     if (!user) return res.status(403).json({ msg: 'User not found' });
 
     req.userId = user._id;
@@ -20,16 +19,22 @@ exports.checkAuth = async (req, res, next) => {
   }
 };
 
-exports.getUserId = (req, res, next) => {
-  if (!req.session.user) {
-    req.userId = null;
+exports.getUserId = async (req, res, next) => {
+  req.userId = null;
+
+  if (!req.session.user || !isValidObjectId(req.session.user)) {
     return next();
   }
 
-  User.findOne({ _id: req.session.user }, '_id', (error, user) => {
-    if (error || !user) return res.status(400).json();
+  try {
+    const user = await User.findById(req.session.user).select('_id').exec();
 
-    req.userId = user._id;
+    if (user) {
+      req.userId = user._id;
+    }
+
     next();
-  });
+  } catch (error) {
+    res.status(500).json({ msg: 'Error, please try again later' });
+  }
 };
