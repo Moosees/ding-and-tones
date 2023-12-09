72cd6cd79698253881c9531db79294d6fc9ec925
Moosees <linus.almgren@gmail.com>
2023-12-09 23:57:00 +0100

    update checkAuth function to async await

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,23 @@
 const User = require('../models/user');
+const { isValidObjectId } = require('mongoose');
 
-exports.checkAuth = (req, res, next) => {
-  if (!req.session.user)
+exports.checkAuth = async (req, res, next) => {
+  console.log(req.session.user);
+  if (!req.session.user || !isValidObjectId(req.session.user)) {
     return res.status(403).json({ msg: 'No authorization' });
+  }
 
-  User.findOne({ _id: req.session.user }, '_id', (error, user) => {
-    if (error || !user) return res.status(403).json({ msg: 'User not found' });
+  try {
+    const user = await User.findById(req.session.user).select('_id').exec();
+
+    console.log('checkAuth', user);
+    if (!user) return res.status(403).json({ msg: 'User not found' });
 
     req.userId = user._id;
     next();
-  });
+  } catch (error) {
+    res.status(500).json({ msg: 'Error, please try again later' });
+  }
 };
 
 exports.getUserId = (req, res, next) => {
