2a78d6a7757813fcc8eefc19c867d2254ef43b3d
Moosees <linus.almgren@gmail.com>
2023-12-12 01:39:05 +0100

    refactor parseScaleRespone a tiny bit

--- utils/scale.js
+++ utils/scale.js
@@ -1,13 +1,10 @@
 exports.parseScaleResponse = (scaleObject, userId) => {
-  const {
-    _id,
-    info,
-    notes: { dings, round, extra },
-    author,
-  } = scaleObject;
+  const { _id, info, notes, author } = scaleObject;
+  const { dings, round, extra } = notes;
+
   return {
     info,
-    isOwner: userId ? userId.equals(author) : false,
+    isOwner: userId && author ? userId.equals(author) : false,
     notes: { dings: dings || [], round: round || [], extra: extra || [] },
     scaleId: _id,
   };
