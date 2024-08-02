602445ec8c65d01d4e93ae31b35205e7c4bb01ee
Linus Almgren <linus.almgren@gmail.com>
2024-08-02 23:54:39 +0200

    delete useDefaultState effect

--- client/src/components/app/effects/AppEffects.jsx
+++ client/src/components/app/effects/AppEffects.jsx
@@ -1,10 +1,8 @@
-import useDefaultState from './useDefaultState';
 import useKeyboardForDropdown from './useKeyboardForDropdown';
 import usePlayWithKeyboard from './usePlayWithKeyboard';
 import useResetCurrentBeat from './useResetCurrentBeat';
 
 const AppEffects = () => {
-  useDefaultState();
   useResetCurrentBeat();
   usePlayWithKeyboard();
   useKeyboardForDropdown();
