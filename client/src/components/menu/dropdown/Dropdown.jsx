695a1a61239d723b87d3efd5b5f4159e653b394e
Linus Almgren <linus.almgren@gmail.com>
2024-09-23 23:19:22 +0200

    add navigation button in user menu to go back to start page

--- client/src/components/menu/dropdown/Dropdown.jsx
+++ client/src/components/menu/dropdown/Dropdown.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import useCloseOnEsc from '../../../hooks/useCloseOnEsc';
 import useCloseOutside from '../../../hooks/useCloseOutside';
+import BtnMenu from '../../shared/button/BtnMenu';
 import Account from '../account/Account';
 import SignIn from '../signIn/SignIn';
 import Sound from '../sound/Sound';
 import { DropdownContainer } from './dropdown.styles';
-// import BtnMenu from '../shared/button/BtnMenu';
 
 const Dropdown = ({ btnRef, isOpenCb }) => {
   useCloseOnEsc(() => isOpenCb(false));
   const { insideRef } = useCloseOutside(isOpenCb, btnRef);
+  const navigate = useNavigate();
 
   return (
     <DropdownContainer ref={insideRef}>
+      <BtnMenu label="Home" icon="help_outline" onClick={() => navigate('/')} />
       <Sound />
-      {/* <BtnMenu label="Support me" icon="support" /> */}
-      {/* <BtnMenu label="Help" icon="help_outline" /> */}
       <Account />
       <SignIn />
     </DropdownContainer>
